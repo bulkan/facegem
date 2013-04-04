794905dde098828007be9ba172ab1cd9da727655
Bulkan Evcimen <bulkan@gmail.com>
2013-04-04 18:15:10 +1100

    using Webmock to help test this

--- spec/facegem_spec.rb
+++ spec/facegem_spec.rb
@@ -1,12 +1,21 @@
 require "facegem"
+require "webmock/rspec"
+
+include WebMock::API
 
 describe GraphAPI do
+
+  before do
+    stub_request(:get, "https://graph.facebook.com//me?access_token=").
+    to_return(:status => 200, :body => "", :headers => {})
+
+  end
+
   it "instantiates correctly" do
     GraphAPI.new
   end
 
   it "I should be able to GET" do
-    # need to mock httparty.get... somehow
 
     graph = GraphAPI.new(access_token="")
     graph.get('/me')
